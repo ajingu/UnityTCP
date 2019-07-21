2bf8ccde8be9edf2bacb9755a3081d7fd1efbb06
Arata Jingu <guujin0120@gmail.com>
2019-07-21 11:10:44 +0900

    Remove unused namespaces

--- Assets/Scripts/Server/IntServer.cs
+++ Assets/Scripts/Server/IntServer.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-public class IntServer : Server
+﻿public class IntServer : Server
 {
     protected override void OnMessage(string msg)
     {
